7eaa407e3256b39f502d74680c394d3fb6f19113
Charley Sanchez <charleysanchez7@gmail.com>
2025-05-10 11:54:06 -0700

    finaliΩed dataset creation

--- src/utils.py
+++ src/utils.py
@@ -5,21 +5,21 @@ def train_test_val_splits():
         split_file='data/xray_images/train.txt',
         image_root_dir='data/xray_images/',
         bbox_csv='data/xray_images/BBox_List_2017.csv',
-        patient_csv='data/xray_images/Data_Entry_2017.csv'
+        metadata_csv='data/xray_images/preprocessed_metadata.csv'
     )
 
     val = ChestXrayDataset(
         split_file='data/xray_images/val.txt',
         image_root_dir='data/xray_images/',
         bbox_csv='data/xray_images/BBox_List_2017.csv',
-        patient_csv='data/xray_images/Data_Entry_2017.csv'
+        metadata_csv='data/xray_images/preprocessed_metadata.csv'
     )
 
     test = ChestXrayDataset(
         split_file='data/xray_images/test_list.txt',
         image_root_dir='data/xray_images/',
         bbox_csv='data/xray_images/BBox_List_2017.csv',
-        patient_csv='data/xray_images/Data_Entry_2017.csv'
+        metadata_csv='data/xray_images/preprocessed_metadata.csv'
     )
 
     return {
