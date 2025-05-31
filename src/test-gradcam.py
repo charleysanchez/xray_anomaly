5330a7491f210fc609ef4e2e308e20354e3699ea
charleysanchez <charleysanchez7@gmail.com>
2025-05-30 17:19:15 -0700

    adding in the gradcam

--- src/test-gradcam.py
+++ src/test-gradcam.py
@@ -4,6 +4,7 @@ from PIL import Image
 import cv2
 from gradcam import GradCAM
 import numpy as np
+import os
 
 NUM_CLASSES = 14
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -27,6 +28,7 @@ preprocess = transforms.Compose([
                          std=[0.229, 0.224, 0.225])
 ])
 img_path = 'data/xray_images/images_001/images/00000001_000.png'
+img_name = os.path.basename(img_path)
 img = Image.open(img_path).convert('RGB')
 input_tensor = preprocess(img).unsqueeze(0).to(device)
 
@@ -36,7 +38,7 @@ heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
 heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
 orig = np.array(img.resize((224, 224)))
 overlay = np.uint8(0.5 * heatmap + 0.5 * orig)
-out_path = 'gradcam_overlay.png'
+out_path = f'gradcam_images/{img_name}'
 cv2.imwrite(out_path, cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR))
 print(f"Saved Grad-CAM overlay to {out_path}")
 
